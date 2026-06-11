5d87f4d7a9532d8ea153db29201301b77c982ebc
BelliaNino <bellianino1996@gmail.com>
2026-06-11 23:09:05 +0200

    add jumbotron to notfound

--- src/layouts/Structure.jsx
+++ src/layouts/Structure.jsx
@@ -38,7 +38,7 @@ function Structure() {
                 <button className="btn btn-sm btn-dark text-white">Prodotti</button>
               </Link>
               <Link to="/NotFound">
-                <button className="btn btn-sm btn-dark text-white">Test</button>
+                <button className="btn btn-sm btn-dark text-white">Chi Siamo</button>
               </Link>
               <input
                 className="form-control form-control-sm border-0 text-white"
@@ -52,7 +52,9 @@ function Structure() {
         </nav>
       </header>
 
-      <Outlet />
+      <main>
+        <Outlet />
+      </main >
 
       <footer className="bg-warning custom-footer px-4 py-4 mt-auto border-top">
         <div className="container-fluid d-flex flex-column flex-md-row justify-content-between align-items-center gap-3">
