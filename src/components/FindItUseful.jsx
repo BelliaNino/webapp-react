70bfbbeb6ee5d7a1ababa96aa461421611b6198b
“Nilde <nilde.benedetti@gmail.com>
2026-06-15 18:59:09 +0200

    aggiunta classe dinamica per colore icona quando liked

--- src/components/FindItUseful.jsx
+++ src/components/FindItUseful.jsx
@@ -21,7 +21,7 @@ function FindItUseful({ reviewLikes }) {
             <span className="like-btn spx-2" onClick={clickHandler}>
                 <i className="bi bi-hand-thumbs-up-fill"></i>
             </span>
-            <p className="date-text small text-black-50 py-1 m">{findItUsefulCount} {findItUsefulCount > 1 || findItUsefulCount === 0
+            <p className={`date-text small text-black-50 py-1 ${isLiked ? "btn-liked" : '' }`}>{findItUsefulCount} {findItUsefulCount > 1 || findItUsefulCount === 0
                 ? "persone l'hanno trovata utile"
                 : "persona l'ha trovata utile"}</p>
         </div>
